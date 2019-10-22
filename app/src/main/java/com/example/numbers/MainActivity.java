e23f776fb75d1f554ce73e0e7c87d2d9220dfc17
miia <miia@com.tw>
2019-10-22 15:39:59 +0800

    1022

--- app/src/main/java/com/example/numbers/MainActivity.java
+++ app/src/main/java/com/example/numbers/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.numbers;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Random;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,12 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView number;
     int count;
+    int secret = new Random().nextInt(10)+1;
+    String TAG = MainActivity.class.getSimpleName();
+    private ImageView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        Log.d(TAG,"secret"+secret);
+        result = findViewById(R.id.result_image);
         setSupportActionBar(toolbar);
         number = findViewById(R.id.num);
 
@@ -30,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 count++;
                 number.setText(String.valueOf(count));
+                result.setAlpha(0.1f);
+                result.setVisibility(View.VISIBLE);
+                if(count == secret){
+                    Toast.makeText(MainActivity.this,"haha",Toast.LENGTH_LONG).show();
+                    result.setImageResource(R.drawable.shocked);
+
+                }else{
+                    result.setImageResource(R.drawable.smile);
+                    result.animate().alpha(0.0f).setDuration(1200);
+                }
             }
 
         });
